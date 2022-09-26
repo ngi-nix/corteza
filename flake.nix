d150064a1b1e453e42572c796d299b7c19d4c8da
a-kenji <aks.kenji@protonmail.com>
2022-09-26 15:37:09 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:flake.nix

--- flake.nix
+++ flake.nix
@@ -3,22 +3,22 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs;
-    dream2nix.url = github:nix-community/dream2nix;
+    nixpkgs.url = "github:nixos/nixpkgs";
+    dream2nix.url = "github:nix-community/dream2nix";
 
-    server.url = github:cortezaproject/corteza-server;
+    server.url = "github:cortezaproject/corteza-server";
     server.flake = false;
 
-    compose.url = github:cortezaproject/corteza-webapp-compose;
+    compose.url = "github:cortezaproject/corteza-webapp-compose";
     compose.flake = false;
 
-    workflow.url = github:cortezaproject/corteza-webapp-workflow;
+    workflow.url = "github:cortezaproject/corteza-webapp-workflow";
     workflow.flake = false;
 
-    one.url = github:cortezaproject/corteza-webapp-one;
+    one.url = "github:cortezaproject/corteza-webapp-one";
     one.flake = false;
 
-    admin.url = github:cortezaproject/corteza-webapp-admin/2022.3.4;
+    admin.url = "github:cortezaproject/corteza-webapp-admin/2022.3.4";
     #admin.url = git+file:./corteza-webapp-admin;
     admin.flake = false;
   };
@@ -68,23 +68,73 @@
                 };
               };
             };
+          admin =
+            let flake = inputs.dream2nix.lib.makeFlakeOutputs {
+              systems = [ system ];
+              config.projectRoot = ./.;
+              source = ./.;
+
+              # configure package builds via overrides
+              # (see docs for override system below)
+              packageOverrides = {
+                # name of the package
+                corteza-webapp-admin = {
+                  # name the override
+                  add-pre-build-steps =
+                    let
+                      git-tag = pkgs.writeScriptBin "git" ''
+                        echo 2022.3.4
+                      '';
+                    in
+                    {
+                      # update attributes
+                      buildInputs = old: old ++ [ git-tag ];
+
+                      preBuild = "echo hello";
+                    };
+                };
+              };
+            };
             in
             flake.packages.x86_64-linux.default;
+          workflow =
+            let flake = inputs.dream2nix.lib.makeFlakeOutputs {
+              systems = [ system ];
+              config.projectRoot = ./.;
+              source = workflow;
+
+              # configure package builds via overrides
+              # (see docs for override system below)
+              packageOverrides = {
+                # name of the package
+                corteza-webapp-admin = {
+                  # name the override
+                  add-pre-build-steps =
+                    let
+                      git-tag = pkgs.writeScriptBin "git" ''
+                        echo 2022.3.4
+                      '';
+                    in
+                    {
+                      # update attributes
+                      buildInputs = old: old ++ [ git-tag ];
+
+                      preBuild = "echo hello";
+                    };
+                };
+              };
+            };
+            in
+            flake.packages.x86_64-linux.workflow;
 
           corteza = pkgs.callPackage ./corteza params;
-          releasesURL = "https://releases.cortezaproject.org/files";
-
-          webapp = app: "${releasesURL}/corteza-webapp-${app}-${version}.tar.gz";
-          compose = fetchurl { url = webapp "compose"; sha256 = "sha256-2DPzWmoFfnIfCi/VLATyD4DfzMx+NkRwqGPOZoOxFrg="; };
-          workflow = fetchurl { url = webapp "workflow"; sha256 = "sha256-VuViU2twRMx0/bpamUZUt16XanK6rmHRaa8pm4WVTus="; };
-          one = fetchurl { url = webapp "one"; sha256 = "sha256-M4R5aaDh9bBkjlCJA7jOUwgFGyzrPhMbAIBCk68EtTg="; };
 
           # privacy app missing
           # reporter app missing
           # discovery app missing
         in
         {
-          inherit server admin corteza;
+          inherit server admin corteza workflow;
         };
       devShells.x86_64-linux.default = pkgs.mkShell { };
       defaultPackage.x86_64-linux = self.packages.x86_64-linux.corteza;
